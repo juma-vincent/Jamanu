19417fbcbf07d10a4a1441fce0fea5b3088ae51e
juma-vincent <vinsoncode@gmail.com>
2020-12-17 19:58:08 +0300

    added index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>res.send('Good evening'));
+app.get('/', (req, res)=>res.send('Good evening Juma'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
