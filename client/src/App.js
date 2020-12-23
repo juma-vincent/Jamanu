39b583458a3bc9cce78c0c5183eef3d5c3418e08
juma-vincent <vinsoncode@gmail.com>
2020-12-23 06:40:48 +0300

    added react client and heroku build scripts

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"  >
+      <div className="header" style={{height:'70px', backgroundColor:'#DAF7A6 ',
+    position:'relative'
+    }}>
+
+     <div style={{color:'Orange', fontSize:'29px', paddingTop:'7px',  fontWeight:'bold' 
+    }}>Jamanu Food Lover's Market</div>
+      <a href='http://localhost:5000/auth/google'><button style={{
+        padding:'7px', backgroundColor:'#0FB213',
+         color:'white',
+         border:'none',
+         outline: 'none',
+         position:'absolute',
+         top:'20px',
+         right: '16px',
+         cursor: 'pointer'
+         }}>Login with Google</button></a>
+
+       
+        
+      </div>
+      
+
+    </div>
+  );
+}
+
+export default App;
