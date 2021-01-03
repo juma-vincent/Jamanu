3e1e14b5cc0f376b45172644ab6d6f42037106f4
juma-vincent <vinsoncode@gmail.com>
2021-01-03 14:27:17 +0300

    modified index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ require('./routes/productRoutes')(app);
 
 
 if(process.env.NODE_ENV === 'production'){
-// Express will serve up Production assests like our main.js file, or main.css file
+// Express will serve up Production assets like our main.js file, or main.css file
 app.use(express.static('client/build'));
 
 //Express will serve up the index.html file if it doesn't recognise the route
