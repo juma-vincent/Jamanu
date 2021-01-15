5c8f3cf42c3effee5b3dc329b40699cf13218777
juma-vincent <vinsoncode@gmail.com>
2021-01-15 08:06:25 +0300

    edited authroutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,8 +10,7 @@ module.exports = (app)=> {
     app.get('/auth/google/callback',
      passport.authenticate('google'),
      (req, res)=>{
-         res.redirect('/dashboard');
-        
+         res.redirect('/dashboard');        
         }
         
      );
