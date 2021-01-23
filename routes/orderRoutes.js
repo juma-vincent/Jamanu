0f77859a27dc2479a430f0937b4b37590627ddcb
juma-vincent <vinsoncode@gmail.com>
2021-01-23 13:19:42 +0300

    fix

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -37,7 +37,9 @@ module.exports = (app)=>{
         // req.body.Body.ResultCode;
         // req.body.Body.ResultDesc;
 
-        res.send({})
+        // res.send({})        
+        res.redirect(307, '/api/new_order');
+        
         
 
        
@@ -50,7 +52,12 @@ module.exports = (app)=>{
     
 })   
 
-    app.post('/api/new_order', requireLogin, getMpesaOauthToken, async(req, res)=>{
+    app.post('/api/new_order',  getMpesaOauthToken, async(req, res)=>{
+        if(req.body.Body){
+            console.log('====MPESA==REDIRECT===BODY======', req.body.Body)
+            return
+        }
+
        const { cartItems, total, mobileNumber } = req.body;       
 
        const lastEight = mobileNumber.substr(mobileNumber.length - 8); // We're getting the last 8 digits
@@ -87,6 +94,15 @@ module.exports = (app)=>{
         const endpoint = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
         auth = "Bearer " + access_token; 
         let datenow = new Date();        
+
+        let d = new Date();
+        d = new Date(d.getTime() - 3000000);
+        var timestamp = d.getFullYear().toString()+""
+        +((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+""
+        +(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+""
+        +(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+""
+        +((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+"00";
+        console.log("REFINED TIMESTAMP =======", timestamp);
                  
         // const year = datenow.getFullYear().toString();
         let addedmonth = datenow.getMonth()+1 //months starts from 0 in Javascript
@@ -107,31 +123,14 @@ module.exports = (app)=>{
         // console.log("Timestamp", timestamp);
         // let valid = (new Date(timestamp)).getTime() > 0;
         // console.log(valid)
-        // const password = new Buffer.from("174379" + "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919" + timestamp).toString('base64')
+        const password = new Buffer.from("174379" + "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919" + timestamp).toString('base64')
         // console.log("--------===================USER ===NEW ===========mobileNumber")
         
         // console.log("Password", password)
-// =====================================================
-        // Date.prototype.yyyymmdd = function() {
-        //     var mm = this.getMonth() + 1; // getMonth() is zero-based
-        //     var dd = this.getDate();
-          
-        //     return [this.getFullYear(),
-        //             (mm>9 ? '' : '0') + mm,
-        //             (dd>9 ? '' : '0') + dd
-        //            ].join('');
-        //   };
-          
-        //   var date = new Date();
-        //   date.yyyymmdd();
-        //   =======================
 
 
 
-        let d = new Date();
-        d = new Date(d.getTime() - 3000000);
-        var timestamp = d.getFullYear().toString()+""+((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+""+(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+""+(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+""+((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+"00";
-        console.log("REFINED TIMESTAMP =======", timestamp);
+       
        
         request(
                     {
@@ -159,15 +158,14 @@ module.exports = (app)=>{
                      (error, response, body) =>{
                         if(error){
                             console.log(error)
-                        }
+                        }                        
                         
                         
-                        
-                        res.send(body);  
+                        // res.send(body);  
                         
                     }
                 )
-        res.send(user) ;   
+         
     // const result = updateOrder()
     // if(result){
     //     console.log('===================PAYMENT =====SUCCESSFULL==========')
