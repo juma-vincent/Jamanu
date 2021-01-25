c908458462b374f76a7465be13e0408c4dc7b61d
juma-vincent <vinsoncode@gmail.com>
2021-01-25 16:26:00 +0300

    refactor

--- client/src/redux/user/user.actions.js
+++ client/src/redux/user/user.actions.js
@@ -74,7 +74,10 @@ export const makePayment = ({mobileNumber,cartItems, total}, history)=>
         console.log(res.data); 
         const user= res.data;     
         history.push('/payment_pending');
-        setTimeout(()=>  dispatch(checkOrderUpdate({user}, history)),30000);
+        // setTimeout(()=> 
+        //  dispatch(
+        //      checkOrderUpdate({user}, history)),
+        //      30000);
         
         
         
